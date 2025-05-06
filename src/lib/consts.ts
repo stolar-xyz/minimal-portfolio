b0b0d69bcfcf32fd6c7381ecc85bcbd5fd007126
Andrzej Stolarek <stolar.xyz@gmail.com>
2025-05-07 01:32:56 +0200

    feat: add scroll spy navigation

---
+++ src/lib/consts.ts
@@ -0,0 +1,7 @@
+export const pageSections = [
+  { id: 'intro', label: 'Intro' },
+  { id: 'work', label: 'Work' },
+  { id: 'about', label: 'About Me' },
+  { id: 'contact', label: 'Contact' },
+  { id: 'footer', label: 'Footer' },
+];
