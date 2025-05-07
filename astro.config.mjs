cb768dae94d177158f65172c9fda0540fa4c6a2e
Andrzej Stolarek <stolar.xyz@gmail.com>
2025-05-07 02:08:20 +0200

    feat: add geist mono font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: 'local',
+        name: 'Geist Mono',
+        fallbacks: ['monospace'],
+        optimizedFallbacks: false,
+        cssVariable: '--font-geist-mono',
+        variants: [
+          {
+            weight: 400,
+            style: 'normal',
+            src: ['./src/assets/fonts/GeistMono-Regular.woff2'],
+          },
+        ],
+      },
+    ],
+  },
+});
