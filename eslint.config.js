ae40d7ab2a3f09955db2ebdfc2452334ba005dcd
Andrzej Stolarek <stolar.xyz@gmail.com>
2025-06-06 21:22:01 +0200

    chore(core): update core modules and refactor import sorting rules

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,8 @@ import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import importSort from 'eslint-plugin-simple-import-sort';
 import typescript from 'typescript-eslint';
 
+const coreModules = ['astro:assets', 'astro:prefetch'];
+const importsGroups = [['^node', '^astro', '^@?\\w', '^@/', '^\\.', '\\u0000']];
 const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
@@ -33,19 +35,14 @@ export default defineConfig([
       'import-sort': importSort,
     },
     settings: {
-      'import-x/core-modules': ['astro:assets'],
+      'import-x/core-modules': coreModules,
     },
     rules: {
       'import-x/no-named-as-default-member': 'off',
       'import-x/newline-after-import': ['error', { count: 1 }],
       'import-x/consistent-type-specifier-style': ['error', 'prefer-inline'],
       'import-sort/exports': 'error',
-      'import-sort/imports': [
-        'error',
-        {
-          groups: [['^node', '^astro', '^@?\\w', '^@/', '^\\.', '\\u0000']],
-        },
-      ],
+      'import-sort/imports': ['error', { groups: importsGroups }],
     },
   },
   includeIgnoreFile(gitignorePath),
