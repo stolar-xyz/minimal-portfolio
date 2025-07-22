4a177d9f4b3e814ef7bbb623d082c0ac5dba47f9
Andrzej Stolarek <stolar.xyz@gmail.com>
2025-07-22 22:49:06 +0200

    refactor(core): remove unnecessary utils and consts and add breakpoint const

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,7 +1,1 @@
-export const pageSections = [
-  { id: 'intro', label: 'Intro' },
-  { id: 'work', label: 'Work' },
-  { id: 'about', label: 'About Me' },
-  { id: 'contact', label: 'Contact' },
-  { id: 'footer', label: 'Footer' },
-];
+export const MIN_XL_BREAKPOINT = `(min-width: 1280px)`;
