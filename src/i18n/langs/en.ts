bffcab7b7ae1152f19b2bf879467673eb463da47
Andrzej Stolarek <stolar.xyz@gmail.com>
2025-07-22 22:46:54 +0200

    feat(core): change translations and update i18n utils

--- src/i18n/langs/en.ts
+++ src/i18n/langs/en.ts
@@ -1,6 +1,14 @@
 import { type Translations } from '../types';
 
 export default {
-  description:
-    'I specialize in React, TypeScript and accessibility. I build fast, thoughtful web applications.',
+  switchTheme: 'Switch theme',
+  switchLanguage: 'Switch language',
+  openToCollaboration: 'Open to collaboration',
+  sections: [
+    { title: 'Intro', content: 'lorem ipsum' },
+    { title: 'Work', content: 'lorem ipsum' },
+    { title: 'About Me', content: 'lorem ipsum' },
+    { title: 'Contact', content: 'lorem ipsum' },
+    { title: 'Footer', content: 'lorem ipsum' },
+  ],
 } satisfies Translations;
